3f73b69502eae77944625c3b5efe4eb8cefe0aae
Cary Phillips <cary@ilm.com>
2021-08-26 16:37:38 -0700

    cleanup
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- src/vec.h
+++ src/vec.h
@@ -8,23 +8,17 @@ namespace Test {
 template <class T> class Vec
 {
   public:
-
-    T x;
-
     constexpr Vec (const Vec& v) noexcept;
-
     template <class S>  constexpr Vec (const Vec<S>& v) noexcept;
 };
 
 template <class T>
 constexpr inline Vec<T>::Vec (const Vec& v) noexcept
-    : x(v.x)
 {
 }
 
 template <class T> template <class S>
 constexpr inline Vec<T>::Vec (const Vec<S>& v) noexcept
-    : x(T(v.x))
 {
 }
 
