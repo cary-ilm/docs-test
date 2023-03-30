6dc6d5fb917c28449d649145c74ec1beadbd8568
Cary Phillips <cary@ilm.com>
2023-03-29 18:09:32 -0700

    commit
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- scripts/pre_install.sh
+++ scripts/pre_install.sh
@@ -9,9 +9,10 @@ env
 echo pre_install:
 
 echo $READTHEDOCS_OUTPUT
-find $READTHEDOCS_OUTPUT/.. -name index.rst
+find ${READTHEDOCS_OUTPUT}.. -name index.rst
 
-JPG=$(READTHEDOCS_OUTPUT)GammaChart.jpg
+echo "set JPG"
+JPG=${READTHEDOCS_OUTPUT}GammaChart.jpg
 
 wget https://raw.githubusercontent.com/AcademySoftwareFoundation/openexr-images/v1.0/TestImages/GammaChart.exr
 convert GammaChart.exr $JPG
